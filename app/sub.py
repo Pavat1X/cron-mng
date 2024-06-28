808fe3120f903d5bf6bac470b161d5543e8576c1
Pavat Pichetsin <pavatpichetsin@outlook.com>
2024-06-28 16:28:57 +0100

    add user functionality

--- app/sub.py
+++ app/sub.py
@@ -3,15 +3,16 @@ import crontab as cron
 
 @click.command(help="view active jobs")
 def view():
-    cron_user = cron.CronTab(user="your_username")
+    cron_user = cron.CronTab(user="oncex")
     for jobs in cron_user:
         click.echo(jobs)
 
 
 @click.command(help="stop specified job or all jobs")
+@click.argument("user", type=click.STRING)
 @click.argument("name", type=click.STRING)
-def stop(name):
-    cron_user = cron.CronTab(user="your_username")
+def stop(user, name):
+    cron_user = cron.CronTab(user=user)
     res = cron_user.remove_all(comment=name)
     cron_user.write()
 
@@ -23,11 +24,12 @@ def stopall():
         
 
 @click.command(help="start a job")
+@click.argument("user", type=click.STRING)
 @click.argument('name', type=click.STRING)
 @click.argument("scriptpath", type=click.STRING)
 @click.argument("time", type=click.STRING)
-def start(name, scriptpath, time):
-    cron_user = cron.CronTab(user="your_username")
+def start(user, name, scriptpath, time):
+    cron_user = cron.CronTab(user=user)
     link = "python3 " + scriptpath
     li_job = cron_user.new(
         command=link,
